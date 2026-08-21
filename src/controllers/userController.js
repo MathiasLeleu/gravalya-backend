4801111214b714ea330ea89fe039417abd6f54f1
Mathias Leleu <mathiasleleupro@gmail.com>
2026-08-21 16:20:00 +0200

    createUser

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -24,7 +24,38 @@ const userController = {
     },
 
     // Create a new user
+    async createUser(req, res) {
+        const { firstName, lastName, email, password, confirmPassword } = req.body;
 
+        if (!firstName || !lastName || !email || !password || !confirmPassword) {
+            return res.status(400).json({ message: 'Les champs Prénom, Nom, Email, Mot de passe et Confirmation du mot de passe sont requis.' });
+        }
+
+        if (password !== confirmPassword) {
+            return res.status(400).json({ message: 'Le mot de passe et sa confirmation ne correspondent pas.' });
+        }
+
+        const existingEmail = await User.findOne({ where: {email: email} });
+        if (existingEmail) {
+            return res.status(409).json({ message: 'Cet email est déjà utilisé.' });
+        }
+
+        const newUser = await User.create({
+            firstName,
+            lastName,
+            email,
+            password,
+        });
+
+        const newUserWithoutPassword = { 
+            firstName: newUser.firstName, 
+            lastName: newUser.lastName, 
+            email: newUser.email, 
+            role: newUser.role };
+        res.status(201).json(newUserWithoutPassword);
+
+    },
+    
     // Update an existing user
 
     // Delete a user
--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ const router = Router();
 
 router.get('/users', userController.showAllUsers);
 router.get('/users/:id', userController.showOneUser);
-// router.post('/users', userController.createUser);
+router.post('/users', userController.createUser);
 // router.patch('/users/:id', userController.updateUser);
 // router.delete('/users/:id', userController.deleteUser);
 
