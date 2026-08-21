caa52f86f27c62b16f23c97c833b154b9abfb320
Mathias Leleu <mathiasleleupro@gmail.com>
2026-08-21 16:00:22 +0200

    userController + user router

--- src/router.js
+++ src/router.js
@@ -1,7 +1,12 @@
 import { Router } from 'express'
+import { userController } from './controllers/userController.js'
 
 const router = Router();
 
-router.get("/", (req, res ) => {res.send("Hello les G@MERS")})
+router.get('/users', userController.showAllUsers);
+router.get('/users/:id', userController.showOneUser);
+// router.post('/users', userController.createUser);
+// router.patch('/users/:id', userController.updateUser);
+// router.delete('/users/:id', userController.deleteUser);
 
 export { router }
